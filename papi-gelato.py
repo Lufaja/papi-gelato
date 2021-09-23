1f90bea97b3075cc21fd9ad53f99a1f78a615d8e
Lukas <Lukas.v.hulst@gmail.com>
2021-09-23 14:00:21 +0200

    papi-gelato.py gemaakt

---
+++ papi-gelato.py
@@ -0,0 +1,49 @@
+def ijshouder(soort):
+    if soort == "a":
+        soort = "hoorntje"
+        return soort
+    elif soort == "b":
+        soort = "bakje"
+        return soort
+
+def stap3(soort, bollen):
+    antwoord = input("Hier is uw " + soort + " met " + bollen + " bolletje(s). Wilt u nog meer bestellen? (Y/N) ").lower()
+    if antwoord == "y":
+        stap1()
+    elif antwoord == "n":
+        print("Bedankt en tot ziens!")
+        exit()
+    else:
+        print("Sorry dat snap ik niet...")
+        stap3(soort, bollen)
+
+
+def stap2(bollen):
+    soort = input("Wilt u deze " + bollen + " bolletje(s) in A) een hoorntje of B) een bakje? ").lower()
+    if soort == "a" or soort == "b":
+        soort = ijshouder(soort)
+        stap3(bollen, soort)
+    else:
+        print("Sorry dat snap ik niet...")
+        stap2(bollen)
+
+
+def stap1():
+    bollen = input("Hoeveel bolletjes wilt u? ")
+    if bollen.isdigit() == False:
+        print("Sorry dat snap ik niet...")
+        stap1()
+    else:
+        bollen1 = int(bollen)
+    if bollen1 <= 3:
+        stap2(bollen)
+    elif bollen1 <=8:
+        print("Dan krijgt u van mij een bakje met " + bollen + " bolletjes.")
+        soort = "bakje"
+        stap3(soort, bollen)
+    elif bollen1 > 8:
+        print("Sorry, zulke grote bakken hebben we niet.")
+        stap1()
+
+print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
+stap1()
